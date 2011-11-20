37437b9ce1836a13b6a1e532c08155f22af491ec
Sergey Kazenyuk <kazenyuk@gmail.com>
2011-11-21 02:38:46 +0400

    Rewrote k-NN so k-d tree stores vector indexes, not vectors itself. Modified kdtree_distance_t and user_rating_accessor_t added.

--- src/knn.hpp
+++ src/knn.hpp
@@ -8,34 +8,43 @@
 #include <itpp/base/mat.h>
 #include <kdtree++/kdtree.hpp>
 
-template <class R, class M>
+template <class M>
 class kdtree_distance_t
 {
 public:
-	kdtree_distance_t(const R &avg_user_ratings, M &resemblance)
-		:m_avg_user_ratings(avg_user_ratings), 
-		m_resemblance(resemblance)
+	typedef float distance_type;
+	
+	kdtree_distance_t(M &resemblance)
+		:m_resemblance(resemblance)
 	{}
-	template <class U>
-	float operator()(const U &user1, const U &user2) const
+	
+	distance_type operator()(size_t user1, size_t user2) const
 	{
-		float c = 0;
-		if (!false)	//TODO: if not present in 'm_resemblance' matrix
-		{
-			c = correlation_coeff(user1, user2, m_avg_user_ratings);
-			m_resemblance(user1, user2) = c;
-			m_resemblance(user2, user1) = c;
-		}
-		else
-		{
-			c = m_resemblance(user1, user2);
-		}
+		distance_type c = m_resemblance(user1, user2);
 		return c*c;
 	}
-	typedef float distance_type;
+	
 private:
-	const R &m_avg_user_ratings;
 	M &m_resemblance;
+};
+
+template <class M>
+class user_rating_accessor_t
+{
+public:
+	typedef typename M::value_type result_type;
+	
+	user_rating_accessor_t(const M &users_ratings)
+		:m_users_ratings(users_ratings)
+	{}
+	
+	result_type operator()(size_t const& v, size_t const n) const
+	{
+		return m_users_ratings.get_row(v)[n];
+	}
+	
+private:
+	const M &m_users_ratings;
 };
 
 template <class M, class V, class R>
@@ -43,34 +52,31 @@ void knn(M &knn_predict, size_t k,
 			const M &users_ratings, R &user_resemblance, 
 			const V &avg_users_rating, const V &avg_product_ratings)
 {
-	//M user_resemblance(user_resemblance_unused.rows(), user_resemblance_unused.cols());
-	//M user_resemblance(user_resemblance_unused);
-	//user_resemblance.zeros();
 	KDTree::KDTree<3, 
-				itpp::vec, 
-				KDTree::_Bracket_accessor<itpp::vec>, //access i-th element of the vector (using operator[]) (result_type operator()(_Val const& V, size_t const N) const)
-				kdtree_distance_t<itpp::vec, itpp::mat>	//squared distance between vectors (distance_type operator() (const _Tp& __a, const _Tp& __b) const)
+				size_t, 
+				user_rating_accessor_t<M>, //access i-th element of the vector (using operator[]) (result_type operator()(_Val const& V, size_t const N) const)
+				kdtree_distance_t<R>	//squared distance between vectors (distance_type operator() (const _Tp& __a, const _Tp& __b) const)
 				> 
-				tree(KDTree::_Bracket_accessor<itpp::vec>(), 
-					kdtree_distance_t<itpp::vec, itpp::mat>(avg_users_rating, user_resemblance));
+				tree(user_rating_accessor_t<M>(users_ratings), kdtree_distance_t<R>(user_resemblance));
 	for (int i=0; i<users_ratings.rows(); ++i)	//users
 	{
-		tree.insert(users_ratings.get_row(i));
+		tree.insert(i);
 	}
 	
 	for (int i=0; i<users_ratings.rows(); ++i)	//users
 	{
 		itpp::mat nearest_neighbours;
-		std::vector<itpp::vec> neighbours;
+		std::vector<size_t> neighbours;
 		
 		// Nearest neighbours of the i-th user
 		std::cout << "neighbours of " << i << " (" << users_ratings.get_row(i) << ")" << std::endl;
-		tree.find_within_range(users_ratings.get_row(i), k, 
-				std::back_insert_iterator<std::vector<itpp::vec>>(neighbours));
+		tree.find_within_range(i, k, 
+				std::back_insert_iterator<std::vector<size_t>>(neighbours));
 		std::for_each(neighbours.begin(), neighbours.end(), 
-					  [&nearest_neighbours](const itpp::vec &v){
-						  std::cout << "neighbour: " << v << std::endl;
-						  nearest_neighbours.append_row(v);
+					  [&nearest_neighbours,&users_ratings](const size_t &v){
+						  itpp::vec neighbour(users_ratings.get_row(v));
+						  std::cout << "neighbour: " << v << " : " << neighbour << std::endl;
+						  nearest_neighbours.append_row(neighbour);
 					});
 		
 		//assert(users_ratings.cols() == nearest_neighbours.cols());
@@ -82,7 +88,9 @@ void knn(M &knn_predict, size_t k,
 										avg_users_rating, i, j, 
 										user_resemblance);
 		}
-		std::cout << "neareast neighbours of " << i << ": " << nearest_neighbours << std::endl;
+		std::cout << "neareast neighbours of " << i 
+				  << " " << users_ratings.get_row(i) << ": " 
+				  << nearest_neighbours << std::endl;
 		std::cout << "knn_predict: " << knn_predict << std::endl;
 	}
 }
